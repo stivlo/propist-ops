c9da8479e7250228fb5eea11c41e343a3bce0450
Stefano Locati <stivlo@gmail.com>
2019-01-25 00:00:48 +0000

    creating hosts file for web host

--- ansible/write-public-ip-for-propist-web.sh
+++ ansible/write-public-ip-for-propist-web.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+echo [web] > web-hosts
 aws ec2 describe-instances --filters "Name=tag:Name,Values=propist-web" \
-  --query 'Reservations[].Instances[].[PublicIpAddress]' | jq -r .[][]
+  --query 'Reservations[].Instances[].[PublicIpAddress]' | jq -r .[][] >> web-hosts
